42a2f92bcf96592e8e0becfe01c4688e6ddeeca6
paidforby <ggallo102@gmail.com>
2018-03-26 07:54:34 -0700

    cleaning up libs and reqs

---
+++ requirements.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mkdir libs
+cd libs
+wget https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.6.0/p5.js
+wget https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.6.0/addons/p5.dom.js
+wget https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.6.0/addons/p5.sound.js
+wget https://paidforby.github.io/LikeAir/libs/midi_control.js
+
